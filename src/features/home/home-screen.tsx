2f91d68936dffb6a3c7c63ff1d6ebb5325a1f06f
johkade <johkade.dev@gmail.com>
2022-11-01 13:45:24 +0100

    feat: set up rnsvg

--- src/features/home/home-screen.tsx
+++ src/features/home/home-screen.tsx
@@ -1,6 +1,7 @@
 import {MotiView} from 'moti';
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import Svg, {Circle} from 'react-native-svg';
 
 import Swipeable from 'react-native-gesture-handler/Swipeable';
 
@@ -14,6 +15,16 @@ export const HomeScreen = () => {
         <Swipeable renderLeftActions={() => <Text>hello</Text>}>
           <Text>Home 🎃</Text>
         </Swipeable>
+        <Svg height="100" width="100" viewBox="0 0 100 100">
+          <Circle
+            cx="50"
+            cy="50"
+            r="45"
+            stroke="blue"
+            strokeWidth="2.5"
+            fill="green"
+          />
+        </Svg>
       </MotiView>
     </View>
   );
